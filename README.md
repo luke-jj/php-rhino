40d6caa46b17d20a5e0b95ad258f68dfb411caf9
Luca J <34938395+luke-jj@users.noreply.github.com>
2019-09-10 06:07:29 +0200

    Add basic project scaffold.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PHP Rhino
 
-
+- routing service
+- middleware
+- error handling
+- debugging
---
+++ lib/rhino.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "/lib/application.php";
+
+function rhino() {
+  return new Application();
+}
