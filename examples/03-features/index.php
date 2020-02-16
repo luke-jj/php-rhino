e80c5260b639741bbc3905f115777ffed7f17ea1
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-02-16 22:36:22 +0100

    Clean up examples.

--- examples/03-features/index.php
+++ examples/03-features/index.php
@@ -4,40 +4,22 @@ require_once (dirname(__FILE__) . "/../../lib/rhino.php");
 
 $app = rhino();
 
-// $app->use($jsonparse, function($req, $res) {
-  // $res->send("This is the second callback.");
-// }, function ($req, $res) {
-  // $res->send("This is the third callback.");
-// });
-
 require_once (dirname(__FILE__) . "/auth.php");
 require_once (dirname(__FILE__) . "/routes/documents.php");
-$app->use('/documents', $auth, $auth, $documentRouter, $auth);
 
-$app->get('/hi', $auth, $auth, function($req, $res) {
+$app->use('/documents', $auth, $documentRouter);
+
+$app->get('/hi', $auth, function($req, $res) {
   $res->send("hi there from base.");
-});
-
-$app->get('/hi', function($req, $res) {
-  $res->send("hi there from base.");
-});
-
-$app->get('/', function($req, $res) {
-  $res->send("hi there from root.");
-  $res->send($req->get('X-Custom-HEAder'));
 });
 
 $app->get('/:number', $auth, function($req, $res) {
   $res->send("You entered " . $req->params['number']);
 });
 
-
-
+$app->get('/customHeader', function($req, $res) {
+  $customHeader = $req->get('X-Custom-Header');
+  $res->send("Received $customHeader");
+});
 
 $app->start();
-
-
-function debug($body) {
-  print_r($body);
-  echo "<br>\n";
-}
